68bbd5f0504bc0104962a6f24db5cbbac0689bfc
Vinay Nandipur Rudraiah <vinay.nandipur@gmail.com>
2019-12-04 11:43:55 +0530

    updated with spring boot source, scm and artifactory integration

---
+++ src/main/java/com/example/devopspoc1/DevopsPoc1Application.java
@@ -0,0 +1,13 @@
+package com.example.devopspoc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPoc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPoc1Application.class, args);
+	}
+
+}
